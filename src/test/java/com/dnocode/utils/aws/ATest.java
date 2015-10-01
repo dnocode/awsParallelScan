0ff9f94d7f0d6d646d267c216d8f020a995de0b7
dnocode <ricceri.dino@gmail.com>
2015-10-01 19:01:13 +0200

    first commit

---
+++ src/test/java/com/dnocode/utils/aws/ATest.java
@@ -0,0 +1,12 @@
+package com.dnocode.utils.aws;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+public class ATest {
+    @Test
+    public void multiplicationOfZeroIntegersShouldReturnZero() {
+
+        assertTrue("tfdfd",true);
+    }
+
+}
